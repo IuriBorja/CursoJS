e39fa1bf8b2ded23e5139bd0b8a091aa8d86a8cf
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-09-08 16:25:49 +0100

    att

---
+++ aula4/comentarios.js
@@ -0,0 +1,12 @@
+//Escrever um comentário longo
+console.log('Hello World!'); // Aqui tem outro comentário
+console.log('Linha 1');
+//console.log('Linha 2);
+
+/*
+console.log('Linha 1');
+console.log('Linha 1');
+console.log('Linha 1');
+console.log('Linha 1');
+console.log('Linha 1');
+*/
